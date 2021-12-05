0d65e88e32efeb9289aa405745a1430186da3a99
JasonSeah <seahjason32500@gmail.com>
2021-12-05 22:45:11 +0800

    Fix/deployment enhancement (#4)
    * Add custom response and update logging config
    
    * Add dump autoload to clean files configs
    
    * Fix config cache
    
    * Fix attempt for cache file
    
    * Fix composer autoload path and update file naming
    
    * Fix php error and cleaning deployment folders
    
    * Attempt to use google secret
    
    * Update deploymemt secret testing
    
    * Add unit test for error custom response
    
    * remove patch code cov

---
+++ tests/Unit/CustomResponseTest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Unit;
+
+use Exception;
+use Illuminate\Support\Facades\Log;
+use Tests\TestCase;
+
+class CustomResponseTest extends TestCase
+{
+    public function test_error_custom_response_without_err_trace()
+    {
+        $response = custom_error_response('test message')->getData();
+
+        $testResponse = json_encode([
+            'message' => 'test message',
+            'err'     => []
+        ]);
+
+        $this->assertJsonStringEqualsJsonString(json_encode($response), $testResponse);
+    }
+
+
+    public function test_error_custom_response_with_err_trace()
+    {
+        $exception = new Exception('test');
+
+        $response = custom_error_response('test message', $exception->getTrace())->getData();
+
+        $testResponse = json_encode([
+            'message' => 'test message',
+            'err'     => $exception->getTrace()
+        ]);
+
+        $this->assertJsonStringEqualsJsonString(json_encode($response), $testResponse);
+    }
+
+    public function test_error_custom_response_without_err_trace_when_production()
+    {
+        app()->detectEnvironment(function () {
+            return 'production';
+        });
+
+        $exception = new Exception('test');
+
+        $response = custom_error_response('test message', $exception->getTrace())->getData();
+
+        $testResponse = json_encode([
+            'message' => 'test message'
+        ]);
+
+        $this->assertJsonStringEqualsJsonString(json_encode($response), $testResponse);
+    }
+
+
+    public function test_error_custom_response_should_log_err_trace_when_production()
+    {
+        app()->detectEnvironment(function () {
+            return 'production';
+        });
+
+        $exception = new Exception('test');
+        custom_error_response('test message', $exception->getTrace())->getData();
+
+        Log::shouldReceive('error')
+            ->with($exception->getTrace());
+    }
+}
